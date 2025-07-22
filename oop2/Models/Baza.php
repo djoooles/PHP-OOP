dcc8888beb9a6b468a0647ae2927c0cc0b0e1dde
Djordje Stojkovic <204479809+djoooles@users.noreply.github.com>
2025-07-22 12:23:08 -0500

    Add files via upload

---
+++ oop2/Models/Baza.php
@@ -0,0 +1,18 @@
+<?php
+
+class B0aza
+{
+    const HOST = "localhost";
+    const DB_USER = "root";
+    const DB_PASSWORD = "";
+    const DB_NAME = "web_shop";
+
+    public $konekcija;
+    public function __construct()
+    {
+        $this->konekcija = mysqli_connect(self::HOST,self::DB_USER,self::DB_USER, self::DB_NAME);
+    }
+}
+const IME = "Zoki";
+
+?>
